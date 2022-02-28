a870ca4343f405d1dd79a5d00b27056bdbcaff99
Arnold Patten <apatten001@yahoo.com>
2022-02-28 06:57:18 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ProductListComponent } from './components/product-list/product-list.component';
+// manually put this for the Http client module 
+import {HttpClientModule} from '@angular/common/http';
+import { ProductService } from './services/product.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
-  providers: [],
+  // allows us to inject that given service into other parts of the application
+  providers: [ProductService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
