ed9cb10f01cca41150d59cdf0015f59aadbc347b
Arnold Patten <apatten001@yahoo.com>
2022-03-01 13:17:31 -0500

    create method to list products from the productService

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -1,28 +1,45 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { Product } from 'src/app/common/product';
 import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list-grid.component.html',
-  //templateUrl: './product-table-list.component.html',
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent implements OnInit {
 
   products: Product[] | undefined;
+  currentCategoryId: number | undefined;
+
   // inject a product service into this product list component
-  constructor(private productService: ProductService) { }
+  // activated route is needed for accessing route parameters
+  constructor(private productService: ProductService,
+              private route:ActivatedRoute) { }
 
   ngOnInit(): void {
 
+    this.route.paramMap.subscribe(()=>
+    this.listProducts());
     // this is where i call my listproducts method
-    this.listProducts();
-
   }
 
   listProducts(){
-    this.productService.getProductList().subscribe(
+    // check if the id parameter is available
+    //route - use the activated route
+    // snapshot is the state of the route of the given moment and time
+    const hasCategoryId: boolean = this.route.snapshot.paramMap.has("id");
+
+    if (hasCategoryId) {
+      // get the id param string(+ in front of number turns it to a string) 
+      this.currentCategoryId = +this.route.snapshot.paramMap.get("id");
+    }
+    else{
+      this.currentCategoryId =1;
+    }
+
+    this.productService.getProductList(this.currentCategoryId).subscribe(
       data => this.products = data);
   }
 
