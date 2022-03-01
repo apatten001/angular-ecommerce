843a1a43fa56da2a82d12f0bb78392bf9601bf78
Arnold Patten <apatten001@yahoo.com>
2022-03-01 13:15:07 -0500

    create method for getProductList and map response to get the products

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -13,8 +13,12 @@ export class ProductService {
   // inject httpCLeint angular injects it for us
   constructor(private httpClient: HttpClient ) { }
 
-  getProductList(): Observable<Product[]> {
-    return this.httpClient.get<GetResponse>(this.baseUrl).pipe(
+  getProductList(theCategoryId:number): Observable<Product[]> {
+
+    // need to build url on category id
+    const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}`
+
+    return this.httpClient.get<GetResponse>(searchUrl).pipe(
       map(response => response._embedded.products)
     )
   }
