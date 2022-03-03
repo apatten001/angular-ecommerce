c6c38c212611abc79da0c7abef577498810d0c61
Arnold Patten <apatten001@yahoo.com>
2022-03-03 16:41:59 -0500

    add (.target).value to the update page size error to fix the error

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -16,7 +16,7 @@ export class ProductListComponent implements OnInit {
   searchMode: boolean = false;
 
   thePageNumber: number =1;
-  thePageSize:number = 10;
+  thePageSize:number = 5;
   theTotalElements:number = 0;
   
 
@@ -96,6 +96,11 @@ export class ProductListComponent implements OnInit {
       this.thePageSize = data.page.size;
       this.theTotalElements = data.page.totalElements;
     }
+  }
+  updatePageSize(pageSize:number){
+    this.thePageSize = pageSize;
+    this.thePageNumber =1;
+    this.listProducts();
   }
 
 }
