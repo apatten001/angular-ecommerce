668b090d4072e5d1e0abc04f14b4a49c56f0499f
Arnold Patten <apatten001@yahoo.com>
2022-03-06 09:07:12 -0500

    make expiration date and year dependent fiels based off the year

--- src/app/components/checkout/checkout.component.ts
+++ src/app/components/checkout/checkout.component.ts
@@ -97,4 +97,31 @@ export class CheckoutComponent implements OnInit {
 
   }
 
+  handleMonthsAndYears(){
+
+    // get the handle to the credit card form group
+    const creditCardFormGroup = this.checkoutFormGroup.get("creditCard")
+
+    const currentYear: number = new Date().getFullYear();
+
+    const selectedYear: number = Number(creditCardFormGroup.value.expirationYear) ;
+
+    // if the current year equals selected year?
+
+    let startMonth: number;
+
+    if (currentYear === selectedYear){
+      startMonth = new Date().getMonth() + 1;
+    }else{
+      startMonth =1;
+    }
+
+    this.luv2ShopFormService.getCreditCardMonths(startMonth).subscribe(
+        data => {
+          console.log("Retrieved credit card months: " + JSON.stringify(data))
+          this.creditCardMonths = data;
+        }
+    );
+  }
+
 }
