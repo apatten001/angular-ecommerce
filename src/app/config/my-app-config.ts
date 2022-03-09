d32d069e897e1de982fbb06a2e268f8e46cee29b
Arnold Patten <apatten001@yahoo.com>
2022-03-09 15:09:48 -0500

    add okta dependencies and configure them for the styles in angular.json

---
+++ src/app/config/my-app-config.ts
@@ -0,0 +1,11 @@
+export default {
+
+    oidc:{
+        clientId:"0oa43lahrddXbod6N5d7", 
+        issuer:"https://dev-26699832.okta.com/oauth2/default",
+        redirectUri:"http://localhost:4200/login/callback",
+        scopes: ["openid", 'profile', 'email']
+
+    }
+
+}
