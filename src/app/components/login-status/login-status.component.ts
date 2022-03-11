de2d019d38a557ff39d18c719b923a30dda4d3ed
Arnold Patten <apatten001@yahoo.com>
2022-03-11 13:45:18 -0500

    create component service and class for order history to display orders

--- src/app/components/login-status/login-status.component.ts
+++ src/app/components/login-status/login-status.component.ts
@@ -11,6 +11,8 @@ export class LoginStatusComponent implements OnInit {
   isAuthenticated: boolean = false;
   userFullName: String;
 
+  storage: Storage = sessionStorage;
+
   constructor(private oktaAuthService: OktaAuthService) { }
 
   ngOnInit(): void {
@@ -30,6 +32,13 @@ export class LoginStatusComponent implements OnInit {
       this.oktaAuthService.getUser().then(
         (result) => {
           this.userFullName =result.name;
+
+          // get users email from response
+          const theEmail = result.email;
+
+          // stores the email in browser storage
+          this.storage.setItem('userEmail', JSON.stringify(theEmail));
+
         }
       )
     }
