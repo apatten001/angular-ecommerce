8cc426700c4cdc38435a0db22e261c119b995441
Arnold Patten <apatten001@yahoo.com>
2022-03-21 15:22:51 -0400

    remove hard coded urls for environment

--- src/app/services/auth-interceptor.service.ts
+++ src/app/services/auth-interceptor.service.ts
@@ -2,6 +2,7 @@ import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/c
 import { Injectable } from '@angular/core';
 import { OktaAuthService } from '@okta/okta-angular';
 import { from, Observable, lastValueFrom, firstValueFrom} from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -17,8 +18,9 @@ export class AuthInterceptorService implements HttpInterceptor{
 
   private async handleAccess(request: HttpRequest<any>, next: HttpHandler): Promise<HttpEvent<any>>{
     // Only add an access token for secured endppoints
+    const theEndpoint = environment.luv2shopApiUrl + '/orders'
 
-    const securedEndpoints = ['http://localhost:8080/api/orders'];
+    const securedEndpoints = [theEndpoint];
 
     if (securedEndpoints.some(url => request.urlWithParams.includes(url))){
 
